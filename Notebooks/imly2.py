c416f3eb9d20ab399a035b3b0b48d36ce1fcf2c7
Shakkeel <shakkeelahammed@gmail.com>
2018-12-22 00:30:48 +0530

    updated imly2.py with complete workflow

--- Notebooks/imly2.py
+++ Notebooks/imly2.py
@@ -20,7 +20,7 @@
 5) Testing performance with datasets
 
 Qs
-+ Are we still planning to use the pred by scikit as the target for Keras?
++ Are we still planning to use the pred by scikit as the target for Keras? - Yes.
 '''
 
 from sklearn.preprocessing import StandardScaler
@@ -37,160 +37,156 @@ from keras.activations import linear
 from keras.wrappers.scikit_learn import KerasRegressor
 import talos as ta
 
+import os
+import json
+import zipfile
+import shutil
+from keras.models import model_from_json
+import onnxmltools
+from talos import Deploy
+import types
 
-def dope(obj, using='dnn'):
-    if using == 'sklearn':
-        return obj
-    elif using == 'dnn':
+
+def dope(obj, **kwargs):
+    ''' Pending -
+    1) Argument generalization
+    2) Extract model name before creating model -- DONE
+    3) Getter and setter for params
+    4) Duplicate file name issue in Deploy() -- FIXED
+    5) compiling right after extractling Talos -- FIXED
+        + How to extract the best model's opt and loss?
+        + Try load_model by Predict()
+    '''
+    p = {   # use get_params
+        'first_neuron': [1],
+        'optimizer': ['SGD', 'nadam'],
+        'losses': [mse],
+        'activation': [linear]
+    }
+    kwargs.setdefault('using', 'dnn')
+    kwargs.setdefault('params', p)
+    if kwargs['using'] == 'dnn':
+        p = kwargs['params']  # use set_params()
+        name = obj.__class__.__name__
+        create_model = get_model_design(name)
         model = KerasRegressor(build_fn=create_model,
                                epochs=10, batch_size=10, verbose=0)
-        # fit_keras(x_train,y_train,model)  
-        obj.fit = fit_keras
-        # obj.fit = model.fit
-        obj.predict = model.predict
-        obj.score = model.score
-        return obj
+        obj = model
+        obj.fit = types.MethodType(fit_keras, obj)
+        obj.save = types.MethodType(save, obj)
+        # obj.fit = fit_keras
+        # obj.predict = model.predict
+        # obj.score = model.score
+        # obj.save = save
+        # return obj
     return obj
 
 
-def create_model():
+def get_model_design(name):
+    mapping = {
+        'LinearRegression': f1
+    }
+
+    for key, value in mapping.items():
+        if key == name:
+            function_name = value
+
+    return function_name
+
+
+def f1():
     model = Sequential()
     model.add(Dense(1, input_dim=1, activation='linear'))
     model.compile(optimizer='adam', loss='mse', metrics=['mse'])
     return model
 
-# def select_fit(obj,using='dnn'):
-#     if using == 'sklearn':
-#         return 
 
-#     elif using == 'dnn':
-#           model = KerasRegressor(build_fn=create_model, epochs=10, batch_size=10, verbose=0)
-#           obj.fit = model.fit
-#           return obj
+def talos_model(x_train, y_train, x_val, y_val, params):
+
+    model = Sequential()
+    model.add(Dense(params['first_neuron'],
+                    input_dim=x_train.shape[1],
+                    activation=params['activation']))
+
+    model.compile(optimizer=params['optimizer'],
+                  loss=params['losses'],
+                  metrics=['acc'])
+
+    out = model.fit(x_train, y_train,
+                    batch_size=params['batch_size'],
+                    epochs=params['epochs'],
+                    verbose=0,
+                    validation_data=[x_val, y_val])
+
+    return out, model
 
 
-# def keras_model(self, x_train, y_train, x_val, y_val, params):
-#     model = Sequential()
-#     model.add(Dense(params['first_neuron'],
-#                     input_dim=x_train.shape[1],
-#                     activation=params['activation']))
-
-#     model.compile(optimizer=params['optimizer'],
-#                     loss=params['losses'],
-#                     metrics=['mse'])
-
-#     out = model.fit(x_train, y_train,
-#                     batch_size=params['batch_size'],
-#                     epochs=params['epochs'],
-#                     verbose=0,
-#                     validation_data=[x_val, y_val])
-
-#     return out, model
-
-# def fit(self, x_train, y_train, using='dnn'):
-#     if using == 'sklearn':
-#         self.model.fit(x_train, y_train)
-#         return self
-#     else:
-#         self.fit_keras_model(x_train, y_train)
-
-# def predict(self, x_test, using='dnn'):
-#     if using == 'sklearn':
-#         return self.model.predict(x_test)
-
-# def score(self, x_test, y_test, using='dnn'):
-#     if using == 'sklearn':
-#         return self.model.score(x_test, y_test)
-
-# def save(self, using='dnn'):
-#     if using == 'sklearn':
-#         filename = 'scikit_model'
-#         pickle.dump(self.model, open(filename, 'wb'))
-
-#     else:
-#         self.save_as_onnx()
-
-def fit_keras(self,x_train, y_train):
+def fit_keras(self, x_train, y_train):
 
     # Hard coding params for the time being. It should ideally be passed
     # as an argument
 
     p = {'lr': (2, 10, 30),
-            'first_neuron': [1],
-            'batch_size': [1, 2, 3, 4],
-            'epochs': [10, 20, 40],
-            'weight_regulizer': [None],
-            'emb_output_dims': [None],
-            'optimizer': ['SGD', 'nadam'],
-            'losses': [mse],
-            'activation': [linear]
-            }
+         'first_neuron': [1],
+         'batch_size': [10],
+         'epochs': [10],
+         'weight_regulizer': [None],
+         'emb_output_dims': [None],
+         'optimizer': ['nadam'],
+         'losses': [mse],
+         'activation': [linear]
+         }
 
     h = ta.Scan(x_train, y_train,
                 params=p,
                 dataset_name='first_linear_regression',
                 experiment_no='a',
-                model=self.model,
+                model=talos_model,
                 grid_downsample=0.5)
 
-# def save_as_onnx():
-    # import os
-    # import json
-    # import zipfile
-    # import shutil
-    # from keras.models import model_from_json
-    # archive = zipfile.ZipFile('linear_regression_firstDataset.zip', 'r')
-    # model_file = archive.open('linear_regression_firstDataset_model.json')
-    # weight_file = archive.open('linear_regression_firstDataset_model.h5')
+    best_model = extract_model(h)
+    best_model.compile(optimizer='adam', loss='mse', metrics=['mse'])
+    # best_model.fit(x_train,y_train)
+    # regressor_model = KerasRegressor(build_fn=best_model,epochs=10, batch_size=10, verbose=0)
+    # self.model = regressor_model
+    self.model = best_model
+    # To delete the zip file that was created by Talos
+    os.remove('./linear_regression_firstDataset.zip')
+    return self.model.fit(x_train, y_train)
 
-    # with zipfile.ZipFile('linear_regression_firstDataset.zip', 'r') as zip_ref:
-    #     zip_ref.extractall('./linear_regression_firstDataset_unzip')
-
-    # # json_file = open('model.json', 'r')
-    # loaded_model_json = model_file.read()
-
-    # # json_file.close()
-    # loaded_model = model_from_json(loaded_model_json)
-    # # load weights into new model
-    # loaded_model.load_weights(
-    #     "./linear_regression_firstDataset/linear_regression_firstDataset_model.h5")
-
-    # shutil.rmtree('./linear_regression_firstDataset_unzip')
-    # print("Loaded model from disk")
-
-    # import onnxmltools
-    # onnx_model = onnxmltools.convert_keras(loaded_model)
+    # Avoid using weights
+    # Re write the whole object as a new keras regressor object while the user calls m.fit()
+    # KerasRegressor's build_fn required a function or an instance of a class as an argument. Hence the above
+    # approach wasn't working(what we have is a keras model - the best_model object)
 
 
-# Test section #
+def extract_model(scan_object):
+    Deploy(scan_object, 'linear_regression_firstDataset', metric='val_loss')
+    archive = zipfile.ZipFile('linear_regression_firstDataset.zip', 'r')
+    model_file = archive.open('linear_regression_firstDataset_model.json')
+    weight_file = archive.open('linear_regression_firstDataset_model.h5')
+
+    with zipfile.ZipFile('linear_regression_firstDataset.zip', 'r') as zip_ref:
+        zip_ref.extractall('./linear_regression_firstDataset_unzip')
+
+    # json_file = open('model.json', 'r')
+    loaded_model_json = model_file.read()
+
+    # json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(
+        "./linear_regression_firstDataset_unzip/linear_regression_firstDataset_model.h5")
+
+    shutil.rmtree('./linear_regression_firstDataset_unzip')
+    print("Loaded model from disk")
+    return loaded_model
 
 
-diabetes = datasets.load_diabetes()
-
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
-sc = StandardScaler()
-diabetes.data = sc.fit_transform(diabetes.data)
-
-x = diabetes_X
-y = diabetes.target
-
-
-# Split the data into training/testing sets
-x_train = diabetes_X[:-20]
-x_test = diabetes_X[-20:]
-
-# Split the targets into training/testing sets
-y_train = diabetes.target[:-20]
-y_test = diabetes.target[-20:]
-
-model = LinearRegression()
-
-m = dope(model)
-
-m.fit(x_train=x_train,y_train=y_train)
-
-# y_pred = m.predict(x_test)
-# score = m.score(x_test,y_test)
-
-# print(score, y_pred)
+def save(self, using='dnn'):
+    if using == 'sklearn':
+        filename = 'scikit_model'
+        pickle.dump(self.model, open(filename, 'wb'))
+    else:
+        onnx_model = onnxmltools.convert_keras(self.model)
+        return onnx_model
